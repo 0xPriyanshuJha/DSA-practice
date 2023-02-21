004837e961f06e3336d934ac959722ff006e187d
priyanshu0931 <priyanshujha2002@gmail.com>
2023-02-21 23:30:32 +0530

    first commit

---
+++ first.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //cout<<"hello world"<<endl;
+    // return 0;
+/* these are data type variables
+int a =123;
+cout<<a<<endl;
+
+char b ='m';
+cout<<b<<endl;
+
+bool bl = true;
+cout<<bl<<endl;
+
+float f = 1.2;
+cout<<f<<endl;
+
+double d = 1.23;
+cout<<d<<endl;
+
+int size = sizeof(a);
+cout<<"size of a is :"<<size<<endl;
+*/
+
+//type casting
+
+int a = 'a';
+cout<<a<<endl;
+
+char ch=98;
+cout<<ch<<endl;
+
+char ch1 = 123456;
+cout<<ch1<<endl;
+
+
+}
+
