9803de0aef0b4100c4ad99e3a496e809da74c362
priyanshu0931 <priyanshujha2002@gmail.com>
2023-02-26 21:16:52 +0530

    third commit

--- conditional.cpp
+++ conditional.cpp
@@ -31,19 +31,133 @@ int main(){
 //     i=i+1;
 // }
 
-int n;
-cin>>n;
+// int n;
+// cin>>n;
 
-int i = 1;
+// int i = 1;
 
-while(i<=n) {
-    int j = 1;
-    while(j<=n){
-        cout<<i;
-        j = j+1;
-    }
-    cout<<endl;
-    i = i+1;
-}
+// while(i<=n) {
+//     int j = 1;
+//     while(j<=n){
+//         cout<<i;
+//         j = j+1;
+//     }
+//     cout<<endl;
+//     i = i+1;
+// }
+// int n;
+// cin>>n;
+
+// int i =1;
+// while (i<=n)
+// {     normal one
+//     int j = 1;
+//     while(j<=n){
+//         cout<<j;
+//         j = j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+// }
+
+// int n;
+// cin>>n;
+
+// int i =1;     backward pattern
+// while(i<=n){
+//     int j =1;
+//     while(j<=n){
+//         cout<<n-j+1;
+//         j=j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+// }
+
+// int n;
+// cin>>n;   count to the 5x5
+
+// int i =1;
+// int count =1;
+// while(i<=n){
+//     int j=1;
+//     while (j<=n)
+//     {
+//         cout<<count<<" ";
+//         count = count+1;
+//         j =j+1;
+//     }
+//     cout<<endl;
+//     i=i+1;
+
+// }
+
+// int n;
+// cin>>n;
+
+// int row =1;
+// while (row<=n)
+// {
+//     int col = 1;
+//     while (col<=row)
+//     {
+//         cout<<"*";
+//         col = col+1;
+//     }
+//     cout<<endl;
+//     row = row+1;
+
+// }
+// int n;
+// cin>>n;
+
+// int row =1;
+// int count = 1;
+// while (row<=n)
+// {
+//     int col = 1;
+//     while (col<=row)
+//     {
+//         cout<<row<<" ";
+//         col = col+1;
+//     }
+//     cout<<endl;
+//     row = row+1;
+    
+// }
+// int n;
+// cin>>n;
+
+// int row =1;
+// int count =1;
+// while (row<=n)
+// {
+//     int col = 1;           side pyramind count wala
+//     while (col<=row)
+//     {
+//         cout<<count<<" ";
+//         count = count+1;
+//         col = col+1;
+//     }
+//     cout<<endl;
+//     row = row+1;
+    
+// }
+
+//  int n;
+//  cin>>n;
+
+//  int i =1;   
+//  while(i<=n){
+//      int j =1;
+//      while(j<=i){
+//          cout<<(i-j+1);
+//          j=j+1;
+//     }
+//      cout<<endl;
+//      i=i+1;
+//  }
+
+
 
 }
